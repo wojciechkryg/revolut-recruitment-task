55f71a765328a77fae44159003b75b3555aa1e8b
Wojciech Kryg <krygwojciech@gmail.com>
2019-12-14 14:23:18 +0100

    Add title

--- feature_rates/src/main/java/com/wojdor/feature_rates/RatesContract.kt
+++ feature_rates/src/main/java/com/wojdor/feature_rates/RatesContract.kt
@@ -1,11 +1,14 @@
 package com.wojdor.feature_rates
 
 import com.wojdor.common.base.BaseContract
+import com.wojdor.domain.Rates
 import com.wojdor.usecase_rates.BaseRatesUsecase
 
 interface RatesContract {
 
-    interface View : BaseContract.View
+    interface View : BaseContract.View {
+        fun showRates(rates: List<Rates>)
+    }
 
     interface Presenter : BaseContract.Presenter<View> {
         val ratesUsecase: BaseRatesUsecase
--- feature_rates/src/main/java/com/wojdor/feature_rates/view/RatesActivity.kt
+++ feature_rates/src/main/java/com/wojdor/feature_rates/view/RatesActivity.kt
@@ -2,6 +2,7 @@ package com.wojdor.feature_rates.view
 
 import android.os.Bundle
 import com.wojdor.common_android.base.BaseMvpActivity
+import com.wojdor.domain.Rates
 import com.wojdor.feature_rates.R
 import com.wojdor.feature_rates.RatesContract
 import org.koin.android.ext.android.inject
@@ -15,5 +16,10 @@ class RatesActivity : BaseMvpActivity<RatesContract.View, RatesContract.Presente
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rates)
+        title = getString(R.string.rates)
+    }
+
+    override fun showRates(rates: List<Rates>) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
