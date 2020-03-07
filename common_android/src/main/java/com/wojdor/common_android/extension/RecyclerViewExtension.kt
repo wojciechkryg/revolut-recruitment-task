739a81e8c916f4f341c0c44f93e2a045bd8ad3bb
Wojciech Kryg <krygwojciech@gmail.com>
2020-03-07 10:44:36 +0100

    Fix scroll to top

--- common_android/src/main/java/com/wojdor/common_android/extension/RecyclerViewExtension.kt
+++ common_android/src/main/java/com/wojdor/common_android/extension/RecyclerViewExtension.kt
@@ -1,7 +1,8 @@
 package com.wojdor.common_android.extension
 
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 fun RecyclerView.scrollToTop() {
-    smoothScrollToPosition(0)
+    (this.layoutManager as LinearLayoutManager).scrollToPositionWithOffset(0, 1)
 }
